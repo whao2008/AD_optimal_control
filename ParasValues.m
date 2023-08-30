06f0d319a4cd95cb7ed5648905efa88b33243604
Wenrui Hao <wenrui.hao@gmail.com>
2023-08-30 12:12:14 -0400

    Add files via upload

---
+++ ParasValues.m
@@ -0,0 +1,50 @@
+% %parameter values
+% paras.initial=[0.01,0,0,0,0];
+% paras.lambda_beta=.08;
+% paras.lambdatauO=.002;
+% 
+% paras.K_Abeta=1;
+% paras.lambda_ptau=0.08;
+% paras.K_ptau=1;
+% paras.lambda_NdTO=.05;
+% paras.lambda_Ndptau=.06;
+% paras.K_Nd=1;
+% paras.lambda_C=0.075;
+% paras.K_C=1;
+% 
+% %paras.lambda_CINd=0.075;
+% %paras.lambda_CMNd=0.1;
+% %paras.K_CI=1;
+% 
+% 
+% paras.alpha1=5;
+% paras.alpha2=.5;
+% paras.alpha=2;
+
+%parameter values
+paras.initial=[1e-3,0,0,0,0];
+paras.lambda_beta=.2;
+paras.K_Abeta=250;
+
+paras.lambda_ptau=0.1;
+paras.K_ptau=200;
+
+paras.lambdatauO=.5;
+
+paras.lambda_NdTO=1e-5;
+paras.lambda_Ndptau=6e-4;
+paras.K_Nd=1;
+
+paras.lambda_CN=.05;
+paras.lambda_C=0.2;
+paras.K_C=100;
+
+%paras.lambda_CINd=0.075;
+%paras.lambda_CMNd=0.1;
+%paras.K_CI=1;
+
+
+paras.alpha1=5;
+paras.alpha2=.5;
+paras.alpha=2;
+
